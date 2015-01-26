8e60be144fb4fec3fdd44d562d0872e34b3d9e00
Milan Nemec <mil.nemec@gmail.com>
2015-01-26 22:04:45 +0100

    add transition and transforms to references

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-
+$(document).ready(function(){
+    var rnd = Math.floor(Math.random() * 9) + 1,
+        welcome = $('.welcome');
+//    welcome.removeClass(/bg*/);
+    welcome.addClass('bg' + rnd);
+    })
